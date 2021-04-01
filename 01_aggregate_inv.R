43ba74e82415e4d4b5d9f755bee55837eda54cb2
samir <samir.arman@gmail.com>
2021-04-01 09:07:49 -0300

    Update 01_aggregate_inv.R

--- 01_aggregate_inv.R
+++ 01_aggregate_inv.R
@@ -1,14 +1,16 @@
 library(tidyverse)
 
+# Leitura dos valores mensais do IPCA
 infl_raw <-
   read_delim(
     "./raw-data/ipca.csv",
     skip = 1,
-    n_max = 36 ,
+    n_max = 36,
     delim = ";",
     col_names = c("month_year", "variation")
   )
 
+# Correção dos dados
 infl <-
   infl_raw %>%
   mutate(
@@ -19,42 +21,37 @@ infl <-
   ) %>%
   relocate(month, year)
 
+# Criação do valor do deflator
 defl <- numeric(nrow(infl))
-
 for (i in 1:nrow(infl)) {
   defl[i] <- prod(infl$multiplier[i:nrow(infl)])
 }
 
-infl$defl <- defl
-
-
 # Carregando os dados de movimentação de estoque
-inv <- read_csv2(
+inventory <- 
+  read_csv2(
   "./raw-data/inventory.csv",
   col_types = cols(
     ano = col_double(),
     mes = col_double(),
     cat_prod = col_factor(),
     vrsai = col_double(),
-    qtdsai = col_double(),
-    prven = col_double()
-  )
-)
+    qtdsai = col_double()
+  )) 
 
-# Agregação dos dadaos de movimentação de estoque
-inv_agg <-
-  inv %>%
+# Agregação dos dados de movimentação de estoque
+# e cálculo da quantidade mensal média de conjuntos
+# e do preço médio
+inv_agg <- inventory %>% 
   group_by(ano, mes) %>%
   summarise(qty = sum(qtdsai) / 2,
             rev = sum(vrsai),
-            p_mean = rev / qty) %>%
+            p = (rev / qty)) %>%
   ungroup() %>%
   mutate(
-    rev = rev * defl,
-    p_mean = p_mean * defl,
-    margin = rev * 0.44,
-    var_cost = rev * (1 - 0.44)
+    p_defl = p * defl,
+    defl = defl
   )
 
+# Gravação dos dados agregados
 write_csv2(inv_agg, "./generated-data/inv_aggregate.csv")
-
