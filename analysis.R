80f5163fcb49083eb53a9703009bd91606976154
samir <samir.arman@gmail.com>
2021-05-19 10:20:34 -0300

    Update variables names

--- analysis.R
+++ analysis.R
@@ -1,5 +1,3 @@
-# Este arquivo contém o código para filtragem
-# e geração das tabelas de resultados
 library(tidyverse)
 
 grid <- read_csv2("./results/simulation.csv") %>%
@@ -8,16 +6,14 @@ grid <- read_csv2("./results/simulation.csv") %>%
 p0 <- 185
 ad0 <- 2000
 
-# Funções----
-
 # Encontra o preço onde o lucro máximo ocorre
 find_at_max <- function(data) {
   data %>%
     group_by(qr_scen) %>%
-    summarise(max_dp = max(dprofit_tax),
-              p_at_point = p[which.max(dprofit_tax)],
-              ad_at_point = ad[which.max(dprofit_tax)],
-              dcost_at_point = dcost[which.max(dprofit_tax)]) %>%
+    summarise(max_dp = max(dprofit_net),
+              p_at_point = p[which.max(dprofit_net)],
+              ad_at_point = ad[which.max(dprofit_net)],
+              dcost_at_point = dcost[which.max(dprofit_net)]) %>%
     right_join(tibble(qr_scen = 1:3)) %>%
     t
 }
@@ -29,12 +25,10 @@ find_at_min <- function(data) {
     summarise(min_cost = min(dcost),
               p_at_point = p[which.min(dcost)],
               ad_at_point = ad[which.min(dcost)],
-              dprofit_tax = dprofit_tax[which.min(dcost)]) %>%
+              dprofit_net = dprofit_net[which.min(dcost)]) %>%
     right_join(tibble(qr_scen = 1:3)) %>%
     t
 }
-
-# Geração das tabelas ----
 
 tables <- function(scenario) {
 
@@ -53,14 +47,14 @@ tables <- function(scenario) {
   # o preço, mantendo o capital
   opt_increase <- 
     grid %>%
-    filter(qw_scen == scenario & p > p0 & dprofit_tax > 0 & dcost <= 0) %>%
+    filter(qw_scen == scenario & p > p0 & dprofit_net > 0 & dcost <= 0) %>%
     find_at_max
 
   # Encontra o menor capital requerido com lucro igual
   # ao atual
   min_cost <-
     grid %>%
-    filter(qw_scen == scenario & p > p0 & dprofit_tax >0 & dcost <= 0) %>%
+    filter(qw_scen == scenario & p > p0 & dprofit_net >0 & dcost <= 0) %>%
     find_at_min
 
   rbind(max_profit, keep_price, opt_increase, min_cost)
