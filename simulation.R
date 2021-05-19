91c2b759335a7051a814e1e5ae696f7992a9081b
samir <samir.arman@gmail.com>
2021-05-19 10:21:09 -0300

    Update simulation

--- simulation.R
+++ simulation.R
@@ -1,103 +1,51 @@
-library(parallel)
-library(pbmcapply)
 library(tidyverse)
 library(Rcpp)
 library(RcppDist)
+library(parallel)
+library(pbmcapply)
 sourceCpp("functions.cpp")
 
-# Determinação do número de iterações ----
+p <- 150:300
+ad <- seq(2000, 10000, 1000)
+qw_scen <- c(1, 2, 3)
+qr_scen <- c(1, 2, 3)
+grid <- expand_grid(p, ad, qw_scen, qr_scen)
 
-# Gera a tabela dos erros médios absolutos para
-# a variável desejada
-gen_mae <- function(estimate, sample_sizes) {
-  iter <- 50
-  m <- matrix(nrow = length(sample_sizes), ncol = iter)
-  rownames(m) <- as.character(sample_sizes)
+# Estimativas -----
+quotes_won <- grid %>%
+    pmap_dbl(~ quotes_won(..1, ..3))
+quotes_received <- grid %>%
+    pmap_dbl(~ quotes_received(..2, ..4))
+qty <- quotes_received * quotes_won * 14.32117
 
-  for (i in 1:length(sample_sizes)) {
-    m[i, ] <-
-      replicate(
-        iter,
-        make_estimates(185, 2000, iter = sample_sizes[i])[[estimate]]
-      )
-  }
-  apply(m, 1, function(x) {
-    mean(abs(x - 0))
-  })
-}
+estimates <-
+    tibble(grid, quotes_won, quotes_received, qty) %>%
+    mutate(
+        rev = qty * p,
+        cost = qty * 104.8025,
+        drev = rev - rev[p == 185 & ad == 2000],
+        dcost = cost - cost[p == 185 & ad == 2000],
+        dprofit_gross = drev - dcost - (ad - 2000)
+    )
 
-set.seed(123)
-gen_mae("dprofit_tax", seq(5e3, 30e3, 5e3))
-gen_mae("dcost", seq(5e3, 30e3, 5e3))
-
-# Simulação ----
-
-iter <- 25e3
-
-grid <-
-  expand.grid(
-    p = 150:300,
-    ad = seq(2000, 10000, 1000),
-    bws = 1:3,
-    brs = 1:3
-  )
-
+# Simulação ------
 cores <- detectCores()
-
 RNGkind("L'Ecuyer-CMRG")
 set.seed(123)
 mc.reset.stream()
-
-res <- pbmcmapply(
-  make_estimates,
-  grid$p,
-  grid$ad,
-  grid$bws,
-  grid$brs,
-  iter,
-  mc.cores = cores,
-  mc.set.seed = TRUE,
-  ignore.interactive = TRUE
+res <- pbmcmapply(dtax, estimates$drev,
+    iter = 15000,
+    mc.cores = cores,
+    ignore.interactive = TRUE
 )
 
+# Resultados ------
 names <- dimnames(res)[[1]]
 r <- as.data.frame(matrix(unlist(t(res)), nrow = nrow(grid)))
 colnames(r) <- names
 
-# Suavização dos resultados -----
-r_soft <- r %>%
-  group_by(ad, qw_scen, qr_scen) %>%
-  mutate(across(
-    .cols = c(dcost, dprofit, dprofit_tax), #p_out doesn't need softening
-    .fns = ~ lowess(p, .x, f = 2 / 36)$y
-  ))
+estimates$dtax <- r$dtax
+estimates$p_out <- r$p_out
+estimates$dprofit_net <- estimates$dprofit_gross - estimates$dtax
 
-# Checagem dos resultados ----
-head(r_soft)
-
-r_soft %>%
-  #filter(qw_scen == 1 & qr_scen == 1) %>%
-  ggplot(aes(
-    x = p,
-    y = dprofit_tax,
-    color = ad,
-    group = ad
-  )) +
-  geom_line() +
-  geom_line(aes(y = dprofit), linetype = "dashed", alpha = 0.6) +
-  geom_vline(xintercept = 185, color = "red") +
-  facet_grid(qr_scen ~ qw_scen)
-
-
-r_soft %>%
-  filter(qw_scen == 1 & qr_scen == 1) %>%
-  ggplot(aes(
-    x = p,
-    y = 100 * p_out,
-    color = ad,
-    group = ad
-  )) +
-  geom_line() +
-  geom_hline(yintercept = 5, col = "red")
-
-write.csv2(r_soft, "./results/simulation.csv")
+write.csv2(estimates, "./results/simulation.csv")
