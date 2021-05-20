2631815c56e02aa43e2ac804bc7b27969816face
samir <samir.arman@gmail.com>
2021-05-20 00:04:49 -0300

    Update format

--- functions.cpp
+++ functions.cpp
@@ -1,11 +1,10 @@
 /**
- * NOTA: 
- * Os cenários de sensibilidade ao preço são representados
- * da seguinte forma: 1: estimado; 2: menos sensível; 3: mais sensível
- * 
- * Os cenários de efetividade da publicidade são representados
+ * Cenários de sensibilidade ao preço:
+ * 1: estimado; 2: menos sensível; 3: mais sensível
+ *
+ * Cenários de efetividade da publicidade:
  * da seguinte forma: 1: estimado; 2: otimista; 3: pessimista
- * 
+ *
  * Por serem utilizadas específicamente para a simulação,
  * as funções neste arquivo não possuem verificações para erros.
  */
@@ -17,13 +16,10 @@ using namespace Rcpp;
 // Constantes
 double P0 = 185.;
 double AD0 = 2000.;
-
 double QW0 = .275;
 NumericVector QW_R = {-0.03, -0.02, -0.042};
-  
 double QR0 = 135;
 NumericVector  QR_B = {40., 55., 22.};
-
 double REV_MEAN = 309022.3;
 double REV_SD = 42359.15;
 
@@ -41,7 +37,7 @@ double quotes_received (double ad, int qr_scen = 1){
   double b = QR_B[qr_scen - 1];
   double a = QR0 - b * log(AD0);
   return(a + b * log(ad));
-} 
+}
 
 // Calcula o imposto devido dadas 13 amostras do faturamento
 // bruto.
@@ -51,7 +47,7 @@ double tax(NumericVector rev_samples) {
   double rev = rev_samples[12];
   double rate = NA_REAL;
   double disc = NA_REAL;
-  
+
   if(annual_rev > 0 && annual_rev < 180e3) {
     rate = 4.5/100;
     disc = 0;
@@ -71,7 +67,7 @@ double tax(NumericVector rev_samples) {
     rate = 30.0 / 100.;
     disc = 720000.;
   }
-  
+
   double tax = (annual_rev * rate - disc) / annual_rev;
   return(tax * rev);
 }
@@ -80,7 +76,7 @@ double tax(NumericVector rev_samples) {
 List dtax(double drev, int iter = 1000) {
   NumericVector base_tax(iter);
   NumericVector new_tax(iter);
-  
+
   for (int i = 0; i < iter; i++) {
     NumericVector mean_rev =
       rtruncnorm(13, REV_MEAN, REV_SD, 0, R_PosInf);
@@ -88,13 +84,13 @@ List dtax(double drev, int iter = 1000) {
       rtruncnorm(13, REV_MEAN + drev, REV_SD, 0, R_PosInf);
     base_tax[i] = tax(mean_rev);
     new_tax[i] = tax(new_rev);
-  } 
+  }
 
   NumericVector dtax = new_tax - base_tax;
   double med_dtax = mean(na_omit(dtax));
   LogicalVector p_out = is_na(dtax);
-  double prob_out = double (sum(p_out)) / p_out.length(); 
+  double prob_out = double (sum(p_out)) / p_out.length();
 
-  return(List::create(Named("dtax") = med_dtax, 
+  return(List::create(Named("dtax") = med_dtax,
                       _["p_out"] = prob_out));
 }
